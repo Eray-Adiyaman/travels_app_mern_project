3802d24030cd345cfcd6bedaf918a0c2e3218a1b
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-05-30 12:11:54 +0300

    components,few styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-
+import {Container,AppBar,Typography,Grow,Grid} from "@mui/material";
+import travels from "./images/travels.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import styles from "./styles"
 function App() {
   return (
-    <div>
-     <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar sx={styles.appBar} position="static" color="inherit">
+        <Typography sx={styles.heading} variant="h2" align="center">Travels</Typography>
+        <img style={styles.image} src={travels} alt="travels" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                  <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                  <Form />
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import Post from "./Post/Post"
+
+export default function Posts() {
+  return (
+      <>
+        <h1>POSTS</h1>
+        <Post/> 
+        <Post />      
+        </>
+  )
+}
