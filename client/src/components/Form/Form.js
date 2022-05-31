684e5d0e20e53f1b3154294f8af0d3e5d83ebbbd
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-05-31 11:06:41 +0300

    createPost request

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,10 @@
-import {TextField,Button,Typography,Paper} from "@mui/material"
-import { useState } from "react"
-import FileBase from "react-file-base64"
-import styles from "./styles"
+import {TextField,Button,Typography,Paper} from "@mui/material";
+import { useState } from "react";
+import FileBase from "react-file-base64";
+import styles from "./styles";
+import { useDispatch } from "react-redux";
+import { createPost } from "../../redux/actions/posts";
+
 export default function Form() {
   const [postData,setPostData] =useState({
     creator:"",
@@ -10,8 +13,12 @@ export default function Form() {
     tags:"",
     selectedFile:""
   })
+  const dispatch = useDispatch();
 
-  const handleSubmit =()=>{
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+
+    dispatch(createPost(postData))
   }
 
   const clear = ()=>{
@@ -24,7 +31,7 @@ export default function Form() {
           <Typography variant="h6">Create Travel Log</Typography>
           <TextField name="creator" variant="outlined" margin="dense" label="Creator" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData,creator: e.target.value})}></TextField>
           <TextField name="title" variant="outlined" margin="dense" label="Title" fullWidth value={postData.title} onChange={(e)=> setPostData({...postData,title: e.target.value})}></TextField>
-          <TextField name="message" variant="outlined" margin="dense" label="Message" fullWidth value={postData.message} onChange={(e)=> setPostData({...postData,tags: e.target.value})}></TextField>
+          <TextField name="message" variant="outlined" margin="dense" label="Message" fullWidth value={postData.message} onChange={(e)=> setPostData({...postData,message: e.target.value})}></TextField>
           <TextField name="tags" variant="outlined" margin="dense" label="Tags" fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData,tags: e.target.value})}></TextField>
           <div style={styles.fileInput}> <FileBase type="file" multiple={false} onDone={({base64})=>setPostData({...postData,selectedFile:base64})}/> </div>
           <Button sx={styles.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit</Button>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,14 +1,13 @@
-import Post from "./Post/Post"
-import { useSelector } from "react-redux"
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import {Grid,CircularProgress } from "@mui/material";
 
 export default function Posts() {
   const posts = useSelector((state)=> state.posts)
   console.log(posts)
   return (
       <>
-        <h1>POSTS</h1>
-        <Post /> 
-        <Post />      
-        </>
+        
+      </>
   )
 }
