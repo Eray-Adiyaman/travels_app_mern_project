48209f44a77d13f7c0d114a55715bb417bbad003
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-06-01 12:38:21 +0300

    dotenv

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors"
 import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js"
-
+import 'dotenv/config'
 const app = express();
 
 
@@ -12,9 +12,8 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/posts",postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://erayadiyaman:erayadiyaman123321@cluster0.p7pyi.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
         .then(()=> app.listen(PORT,()=> console.log(`Server connection on port:${PORT} succesfull!`))) // im stupid,forgot to add listen
         .catch((error)=> console.log(error.message))
