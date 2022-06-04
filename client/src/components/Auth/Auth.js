c3df38096a538814e9cb7680cac020b73cc82978
Eray-Adiyaman <erayadiyaman@outlook.com>
2022-06-04 12:32:27 +0300

    Auth styles

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,8 +1,9 @@
 import { Avatar, Button, Paper, Grid, Typography, Container } from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import styles from "./styles";
+import styles from "./styles.js";
 import Input from "./Input";
 import { useState } from "react";
+
 export default function Auth() {
   
   const [showPassword,setShowPassword]=useState(false);
@@ -23,12 +24,12 @@ export default function Auth() {
 
   return (
     <Container component="main" maxWidth="xs">
-      <Paper elevation={3}>
-        <Avatar>
+      <Paper sx={styles.paper} elevation={3}>
+        <Avatar sx={styles.avatar}>
             <LockOutlinedIcon />
         </Avatar>
         <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
-        <form onSubmit={handleSubmit}>
+        <form style={styles.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             {
               isSignup && (
@@ -43,7 +44,7 @@ export default function Auth() {
             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
             { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
           </Grid>
-          <Button type="submit" fullWidth variant="contained" color="primary">
+          <Button sx={styles.submit} type="submit" fullWidth variant="contained" color="primary">
             {isSignup ? "Sign Up" : "Sign In"}
           </Button>
         </form>
